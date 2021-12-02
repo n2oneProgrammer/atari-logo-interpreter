d4cff3fbfe72a15b8db1978bb7964315a227090e
jaanonim <mat8mro@gmail.com>
2021-12-02 16:20:42 +0100

    better tests

--- tests/core/lexer.test.js
+++ tests/core/lexer.test.js
@@ -1,95 +1,31 @@
 import Lexer from '../../core/lexer';
+import * as data from './data.json';
+
 
 describe('Lexer', () => {
 
     it("Initalize", () => {
-        const commands = ""
-        let lexer = new Lexer("test", commands);
+        let lexer = new Lexer("test", "");
         expect(lexer).toBeDefined();
     });
 
-    it("Test on 1 command", () => {
-        const commands = "4+4"
-        const tokens = [{
-                type: 'NUMBER',
-                value: '4',
-                pos_start: {
-                    id: 0,
-                    line: 0,
-                    column: 0,
-                    fn: 'test',
-                    ftext: '4+4'
-                },
-                pos_end: {
-                    id: 1,
-                    line: 0,
-                    column: 1,
-                    fn: 'test',
-                    ftext: '4+4'
-                }
-            },
-            {
-                type: 'PLUS',
-                value: null,
-                pos_start: {
-                    id: 1,
-                    line: 0,
-                    column: 1,
-                    fn: 'test',
-                    ftext: '4+4'
-                },
-                pos_end: {
-                    id: 2,
-                    line: 0,
-                    column: 2,
-                    fn: 'test',
-                    ftext: '4+4'
-                }
-            },
-            {
-                type: 'NUMBER',
-                value: '4',
-                pos_start: {
-                    id: 2,
-                    line: 0,
-                    column: 2,
-                    fn: 'test',
-                    ftext: '4+4'
-                },
-                pos_end: {
-                    id: 3,
-                    line: 0,
-                    column: 3,
-                    fn: 'test',
-                    ftext: '4+4'
-                }
-            },
-            {
-                type: 'EOF',
-                value: null,
-                pos_start: {
-                    id: 3,
-                    line: 0,
-                    column: 3,
-                    fn: 'test',
-                    ftext: '4+4'
-                },
-                pos_end: {
-                    id: 4,
-                    line: 0,
-                    column: 4,
-                    fn: 'test',
-                    ftext: '4+4'
-                }
-            }
-        ]
-        const error = null
-
-        let lexer = new Lexer("test", commands);
+    it("Correct command", () => {
+        console.log(data)
+        let lexer = new Lexer("test", data.default[0].command);
         let obj = lexer.run();
-        console.log(obj.tokens[0].pos_start);
-        expect(obj.tokens).toEqual(tokens);
-        expect(obj.error).toEqual(error);
+        expect(obj).toEqual(data.default[0].lexer);
+    })
+
+    it("Correct advance command", () => {
+        let lexer = new Lexer("shell", data.default[1].command);
+        let obj = lexer.run();
+        expect(obj).toEqual(data.default[1].lexer);
+    })
+
+    it("Incorrect advance command", () => {
+        let lexer = new Lexer("shell", data.default[2].command);
+        let obj = lexer.run();
+        expect(obj).toEqual(data.default[2].lexer);
     })
 
 });
