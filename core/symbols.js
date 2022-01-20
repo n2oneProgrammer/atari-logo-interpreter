4c57fac2b1e57cfe3b5ffe05cb50133cb2f4311d
jaanonim <mat8mro@gmail.com>
2022-01-20 13:30:51 +0100

    symbol table

---
+++ core/symbols.js
@@ -0,0 +1,26 @@
+module.exports = class SymbolTable {
+    constructor(parent = null) {
+        this.symbols = {};
+        this.parent = parent;
+    }
+
+    get(name) {
+        if (this.symbols[name] != undefined) {
+            return this.symbols[name];
+        }
+
+        if (this.parent != null) {
+            return this.parent.get(name);
+        }
+
+        return null;
+    }
+
+    set(name, value) {
+        this.symbols[name] = value;
+    }
+
+    remove(name) {
+        delete this.symbols[name];
+    }
+}
