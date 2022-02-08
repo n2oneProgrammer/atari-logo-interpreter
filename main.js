95ec19414e27801aeb618e42d2edbad8a72d0b75
mwawrzen <w.w@op.pl>
2022-02-09 00:26:09 +0100

    Add semicolons and hide menu bar

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
-    mainWindow.loadFile('static/pages/index.html')
-
-}
+    mainWindow.loadFile('static/pages/index.html');
+};
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin')
+        app.quit();
+});
