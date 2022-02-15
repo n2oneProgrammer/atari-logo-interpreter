2a691f20f64637476450e32d6033e5493a543141
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-02-15 18:47:50 +0100

    draw line and change scaling

--- static/js/CanvasManager.js
+++ static/js/CanvasManager.js
@@ -1,3 +1,5 @@
+import DrawableLine from "./drawableLine.js";
+
 class CanvasManager {
     static #instance = null;
 
@@ -11,20 +13,51 @@ class CanvasManager {
     constructor() {
         this.canvasContainer = document.querySelector(".canvasContainer");
         this.canvas = document.querySelector(".canvasContainer canvas");
+        this.centerX = this.canvas.clientWidth / 2;
+        this.centerY = this.canvas.clientHeight / 2;
+        this.scale = 1;
         this.rescalingCanvas();
+        this.drawableObjects = [];
         this.setListeners();
+        this.drawableObjects.push(new DrawableLine(-10, -10, 12, 68, 20, 'red'));
+
+        this.draw()
     }
 
     rescalingCanvas() {
         let width = this.canvasContainer.clientWidth;
         let height = this.canvasContainer.clientHeight;
         console.log(width, height);
+        const xscale = this.canvasContainer.clientWidth / this.canvas.width;
+        const yscale = this.canvasContainer.clientHeight / this.canvas.height;
+        this.centerX *= xscale;
+        this.centerY *= yscale;
+
         this.canvas.width = width;
         this.canvas.height = height;
     }
 
+    draw() {
+        const ctx = this.canvas.getContext('2d');
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        const centerX = this.canvas.clientWidth / 2;
+        const centerY = this.canvas.clientHeight / 2;
+        console.log(this.drawableObjects);
+        this.drawableObjects.forEach(el => el.draw(ctx, centerX, centerY, this.scale));
+    }
+
     setListeners() {
-        window.addEventListener("resize", () => this.rescalingCanvas())
+        window.addEventListener("resize", () => {
+            this.rescalingCanvas();
+            this.draw();
+        });
+        this.canvas.addEventListener("wheel", (e) => {
+            e.preventDefault();
+            this.scale += e.deltaY * -0.001;
+            this.scale = Math.min(Math.max(.125, this.scale), 10);
+            console.log(this.scale);
+            this.draw()
+        });
     }
 
 }
