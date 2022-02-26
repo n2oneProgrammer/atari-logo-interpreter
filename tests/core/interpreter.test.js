bd2223620bf9f12aab646a8b0458ec24132c3036
Jaanonim <mat8mro@gmail.com>
2022-02-26 12:27:43 +0100

    Visit ListNode tests

--- tests/core/interpreter.test.js
+++ tests/core/interpreter.test.js
@@ -157,4 +157,33 @@ describe('Interpreter', () => {
         expect(result.value).toBeInstanceOf(NumberValue);
         expect(result.value.value).toBe(-1);
     });
+
+    it('Visit ListNode', () => {
+        let context = new Context("<global>");
+        context.symbolTable = new SymbolTable();
+        context.symbolTable.set("a", new NumberValue(1));
+        context.symbolTable.set("b", new NumberValue(2));
+        let interpreter = new Interpeter(context);
+        let node = new ListNode([
+            new VarNode(new Token(Token.TYPE.IDENTIFIER, 'a', null, new Position(-1, 0, -1, "fn", "text"))),
+            new VarNode(new Token(Token.TYPE.IDENTIFIER, 'b', null, new Position(-1, 0, -1, "fn", "text")))
+        ]);
+        let result = interpreter.visit(node, context);
+        expect(result.value).toEqual(null);
+        expect(result.error).toEqual(null);
+    });
+
+    it('Visit ListNode error', () => {
+        let context = new Context("<global>");
+        context.symbolTable = new SymbolTable();
+        context.symbolTable.set("a", new NumberValue(1));
+        let interpreter = new Interpeter(context);
+        let node = new ListNode([
+            new VarNode(new Token(Token.TYPE.IDENTIFIER, 'a', null, new Position(-1, 0, -1, "fn", "text"))),
+            new VarNode(new Token(Token.TYPE.IDENTIFIER, 'b', null, new Position(-1, 0, -1, "fn", "text")))
+        ]);
+        let result = interpreter.visit(node, context);
+        expect(result.value).toEqual(null);
+        expect(result.error).toBeInstanceOf(RuntimeError);
+    });
 });
