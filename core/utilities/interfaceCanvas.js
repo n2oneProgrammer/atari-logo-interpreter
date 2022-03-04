0e0cf1236b6d05920cb03a8467681a5ac0ac71c2
mwawrzen <w.w@op.pl>
2022-03-04 21:18:20 +0100

    Add semicolons

--- core/utilities/interfaceCanvas.js
+++ core/utilities/interfaceCanvas.js
@@ -2,11 +2,11 @@ module.exports = class InterfaceCanvas {
     static mainWindow = null;
 
     static createLine(x, y, x2, y2, width, color) {
-        InterfaceCanvas.mainWindow.webContents.send("create-line", {x, y, x2, y2, width, color})
+        InterfaceCanvas.mainWindow.webContents.send("create-line", {x, y, x2, y2, width, color});
     }
 
     static editProcedure(name, agrNames, body, node, context) {
-        InterfaceCanvas.mainWindow.webContents.send("edit-procedure", {name, agrNames, body, node, context})
+        InterfaceCanvas.mainWindow.webContents.send("edit-procedure", {name, agrNames, body, node, context});
     }
 
     static refreshCanvas() {
